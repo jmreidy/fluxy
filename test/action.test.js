58b013f0afc7c07f47c450f9a970dc72c1da3e77
Justin Reidy <jmreidy@rzrsharp.net>
2014-07-14 16:41:20 -0700

    add service actions

--- test/action.test.js
+++ test/action.test.js
@@ -1,9 +1,58 @@
-describe('Fluxy Actions', function () {
+var Fluxy = require('../index');
+var Promise = require('bluebird');
+
+describe.only('Fluxy Actions', function () {
+  var TestActions;
+  var Constants = Fluxy.createConstants({
+    serviceMessages: ['TEST', 'FAIL_EXAMPLE']
+  });
+  beforeEach(function () {
+    TestActions = Fluxy.createActions({
+      serviceActions: [
+        [Constants.TEST, function (result) {
+          var token = Promise.defer();
+          token.resolve('resolved');
+          return token.promise;
+        }],
+        [Constants.FAIL_EXAMPLE, function () {
+          var token = Promise.defer();
+          token.reject('failed');
+          return token.promise;
+        }]
+      ],
+
+      actions: [
+
+
+      ]
+    });
+    Fluxy.start();
+    Sinon.spy(TestActions, 'dispatchAction');
+  });
 
   describe('service actions', function () {
-    it('registers an action handler against the provided Constant');
-    it('hooks up the handler for Constant_COMPLETED');
-    it('hooks up the handler for Constant_FAILED');
+
+    it('registers an action handler against the provided Constant', function (done) {
+      TestActions.test('arg1', 'arg2').then(function () {
+        expect(TestActions.dispatchAction).to.have.been.calledWith(Constants.TEST.value, 'arg1', 'arg2');
+        done();
+      })
+      .catch(done);
+    });
+    it('hooks up the handler for Constant_COMPLETED', function (done) {
+      TestActions.test('arg1', 'arg2').then(function () {
+        expect(TestActions.dispatchAction).to.have.been.calledWith(Constants.TEST_COMPLETED.value, {payload: 'resolved'});
+        done();
+      })
+      .catch(done);
+    });
+    it('hooks up the handler for Constant_FAILED', function (done) {
+      TestActions.failExample().then(function () {
+        expect(TestActions.dispatchAction).to.have.been.calledWith(Constants.FAIL_EXAMPLE_FAILED.value, {error: 'failed'});
+        done();
+      })
+      .catch(done);
+    });
   });
 
   describe('#dispatchAction', function () {
